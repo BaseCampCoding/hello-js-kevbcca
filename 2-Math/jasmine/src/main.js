6252f17582fe13343ad401e4bb36fb5db61210c2
kevochee <kmurillo26@basecampcodingacademy.org>
2025-09-17 11:24:12 -0500

    submitting l2-math

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,2 +1,24 @@
+// let ratePerHour = 60;
+// let dayRate = ratePerHour * 8;
+// let budget = 1000;
+// let workdays = budget / dayRate;
+// let billable = 22;
+// let monthlyDiscount = 20;
 
-  
+let hourlyRate = 60;
+let dayRate = hourlyRate * 8;
+let budget = 1000;
+let workdays = Math.floor(budget / dayRate);
+console.log(workdays);
+let billableDays = 22;
+let monthlyDiscount = 0.2;
+let fullMonths = Math.floor(35 / billableDays);
+console.log(fullMonths);
+let remainingDays = 35 - billableDays;
+console.log(remainingDays);
+let remainingCost = remainingDays * dayRate;
+console.log(remainingCost)
+let discountedMonthlyRate = dayRate * billableDays * (1 - monthlyDiscount);
+console.log(discountedMonthlyRate)
+let totalProjectCost = Math.ceil(discountedMonthlyRate + remainingCost);
+console.log(totalProjectCost);
