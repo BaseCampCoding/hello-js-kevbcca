b9a1fab31a3a2b6d36fdde20a259924fd4e1e81c
kevochee <kmurillo26@basecampcodingacademy.org>
2025-09-17 14:00:35 -0500

    submitting l3-stings

---
+++ 3-Strings/jasmine/src/main.js
@@ -0,0 +1,11 @@
+let line = "Stand so high";
+let frontDoorLetter = line[0];
+
+let frontDoorWord = "SHIRE";
+let frontDoorPassword = frontDoorWord[0].toUpperCase() + frontDoorWord.slice(1,5).toLowerCase();
+
+lineWithWhitespace = "Reins and harness";
+let removeWhiteSpace = lineWithWhitespace.trim();
+let backDoorLetter = removeWhiteSpace[removeWhiteSpace.length -1];
+let backDoorWord = "Horse";
+let backDoorPassword = backDoorWord + ", please";
