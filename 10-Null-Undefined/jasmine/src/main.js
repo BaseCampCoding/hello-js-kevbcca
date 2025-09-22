9a81dfb04c9164b541f8eadeeeb7ab1e884d5cbe
kevochee <kmurillo26@basecampcodingacademy.org>
2025-09-22 10:59:38 -0500

    resubmitting l10-null-undefined

--- 10-Null-Undefined/jasmine/src/main.js
+++ 10-Null-Undefined/jasmine/src/main.js
@@ -1,10 +1,55 @@
 //task1
-let vistorName = "John";
-let vistorAge = 32;
-let visitorTicketId = "12345";
-let visitor = { name: "John", age: 32, ticketId: "12345" };
+let visitorName = "Kaden";
+let visitorAge = 45;
+let visitorTicketId = "Tic48";
+
+let visitor = {
+    name: visitorName, 
+    age:visitorTicketId,
+    tickId:visitorTicketId,
+}
+console.log(visitor);
 
 //task2
-ticketId = null;
+visitor.ticket = null;
+console.log(visitor);
 
 //task3
+let tickets = {
+    "Ticket48": null,
+    "Ticket49": "Walker",
+}
+let ticketIdToCheck = "Ticket48";
+let ticketStatusresult;
+if(!(ticketIdToCheck in tickets)){
+    ticketStatusresult = "unknown ticket id";
+}
+else if(tickets[ticketIdToCheck] == null){
+    ticketStatusresult = "not sold";
+}
+else{
+    ticketStatusresult = "sold to" + tickets[ticketIdToCheck];
+}
+console.log(ticketStatusresult);
+
+//task4
+let simpleTicketStatusResult;
+if(ticketIdToCheck in tickets && tickets[ticketIdToCheck]){
+    simpleTicketStatusResult = tickets[ticketIdToCheck];
+}
+else{
+    simpleTicketStatusResult = "invalid ticket";
+}
+console.log(simpleTicketStatusResult);
+
+//task5
+let visitorWithGtc = {
+    name: "Kaden",
+    gtc: {verison: "v2.0"},
+    name: "Walker"
+}
+let gtcVersion;
+if(visitorWithGtc.gtc && visitorWithGtc.gtc.verison){
+    gtcVersion = visitorWithGtc.gtc.verison;
+}
+console.log(gtcVersion)
