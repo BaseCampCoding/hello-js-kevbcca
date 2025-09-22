a75588e3fe94b18d3f52125e74ab514293e3e4ca
kevochee <kmurillo26@basecampcodingacademy.org>
2025-09-22 10:07:35 -0500

    resubmitting l9-objects

--- 9-Objects/jasmine/src/main.js
+++ 9-Objects/jasmine/src/main.js
@@ -1,25 +1,27 @@
 //task1
-let scoreBoard = {"The Best Ever": 1000000}
-console.log(scoreBoard);
+let scoreBoard = {"The Best Ever": 1000000, "Kode": 7000}
 
 //task2
-let newPlayerName = "john";
-let newPlayerScore = 34;
-
+let newPlayerName = "Labron";
+let newPlayerScore = 5000;
+scoreBoard[newPlayerName] = newPlayerScore;
 console.log(scoreBoard);
 
 //task3
-let playerToRemove = 
+let playerToRemove = "The Best Ever";
+delete scoreBoard[playerToRemove];
+console.log(scoreBoard);
 
-//task4
-let playerNameToUpdate = "john";
-let scoreToAdd = 
+//task4 
+let playerNameToUpdate = "Labron";
+let scoreToUpdate = 1000;
+
+scoreBoard[playerNameToUpdate] += 1000;
+console.log(scoreBoard);
 
 //task5
 let mondayBonus = 100;
-for (let i=0; i<scoreBoard.length; i++) {
-    if (scoreBoard[i] == "Monday") {
-        scoreBoard[i] += mondayBonus;
-    }
+for(let player in scoreBoard){
+    scoreBoard[player] += mondayBonus;
 }
-console.log(scoreBoard)
+console.log(scoreBoard);
